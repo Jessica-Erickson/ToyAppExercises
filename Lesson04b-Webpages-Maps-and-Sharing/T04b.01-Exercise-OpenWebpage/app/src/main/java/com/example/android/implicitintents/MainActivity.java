aa328a73277485b1475fd480ba782f74b5fc78ba
Intrepid Panda <jerick999@gmail.com>
2019-03-24 16:59:53 -0600

    In the open webpage button click listener, call openwebpage with red panda network url

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.01-Exercise-OpenWebpage/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.01-Exercise-OpenWebpage/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -39,8 +39,8 @@ public class MainActivity extends AppCompatActivity {
     public void onClickOpenWebpageButton(View v) {
         // Completed (5) Create a String that contains a URL ( make sure it starts with http:// or https:// )
         String urlAddress = "https://www.redpandanetwork.org/";
-        // TODO (6) Replace the Toast with a call to openWebPage, passing in the URL String from the previous step
-        Toast.makeText(this, "TODO: Open a web page when this button is clicked", Toast.LENGTH_SHORT).show();
+        // Completed (6) Replace the Toast with a call to openWebPage, passing in the URL String from the previous step
+        openWebPage(urlAddress);
     }
 
     /**
