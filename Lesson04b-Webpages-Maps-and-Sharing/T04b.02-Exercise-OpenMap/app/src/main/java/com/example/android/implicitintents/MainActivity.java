fe8bbdc9234b28f18bf2cf9234019862819f9840
Intrepid Panda <jerick999@gmail.com>
2019-03-24 20:25:17 -0600

    In showmap, create a new intent with the action type Intent.ACTION_VIEW

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -114,10 +114,10 @@ public class MainActivity extends AppCompatActivity {
 
     // Completed (1) Create a method called showMap with a Uri as the single parameter
     private void showMap(String uri) {
-
+        Intent intent = new Intent(Intent.ACTION_VIEW, );
     }
     // Do steps 2 - 4 within the showMap method
-        // TODO (2) Create an Intent with action type, Intent.ACTION_VIEW
+        // Completed (2) Create an Intent with action type, Intent.ACTION_VIEW
 
         // TODO (3) Set the data of the Intent to the Uri passed into this method
 
